e31c49185267254a7439496ccc2bb5e2367f292f
jwcrosby <jwcrosby89@gmail.com>
2021-08-25 17:39:22 -0600

    render notes

--- main_app/models.py
+++ main_app/models.py
@@ -4,10 +4,9 @@ class Dive(models.Model):
   number = models.IntegerField('Dive Number')
   location = models.CharField(max_length=100)
   max_depth = models.IntegerField()
-  notes = models.TextField(max_length=250)
 
   def __str__(self):
-    return f"#{self.note}: {self.location}"
+    return f"#{self.number}: {self.location}"
 
 class Note(models.Model):
   note = models.TextField(max_length=250)
