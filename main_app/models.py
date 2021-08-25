bab78d16321fbaa4b71029742032569b579e8dfa
jwcrosby <jwcrosby89@gmail.com>
2021-08-25 12:40:39 -0600

    add Dive model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Dive(models.Model):
+  number = models.IntegerField()
+  location = models.CharField(max_length=100)
+  max_depth = models.IntegerField()
+  notes = models.TextField(max_length=250)
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,5 @@
 from django.shortcuts import render
-
-class Dive: 
-  def __init__(self, number, location, max_depth, notes):
-    self.number = number
-    self.location = location
-    self.max_depth = max_depth
-    self.notes = notes
-
-dives = [
-  Dive(1, 'Key Largo', 25, "Pretty not bad"),
-  Dive(4, 'Cozumel', 20, "Pretty not bad"),
-  Dive(25, 'Denver', 50, "Pretty not bad"),
-  Dive(66, 'Pawleys Island', 123, "Pretty not bad")
-]
-
+from .models import Dive
 
 def home(request):
   return render(request, 'home.html')
@@ -22,4 +8,5 @@ def about(request):
   return render(request, 'about.html')
 
 def dives_index(request):
+  dives = Dive.objects.all()
   return render(request, 'dives/index.html', { 'dives': dives })
