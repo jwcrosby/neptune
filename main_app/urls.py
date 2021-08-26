198ab21982b916c6d457a850ef04f44bcf96faf4
jwcrosby <jwcrosby89@gmail.com>
2021-08-25 18:10:28 -0600

    add create dive url

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('dives/', views.dives_index, name='dives_index'),
   path('dives/<int:dive_id>/', views.dives_detail, name='dives_detail'),
+  path('dives/create/', views.DiveCreate.as_view(), name='dives_create'),
   path('dives/<int:dive_id>/add_note/', views.add_note, name='add_note'),
 ]
