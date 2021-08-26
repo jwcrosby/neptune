a5fdfb3889be66f56dcb16fd06c5ebfbd12e0a13
jwcrosby <jwcrosby89@gmail.com>
2021-08-25 18:44:12 -0600

    add Buddy model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Dive, Note
+from .models import Dive, Note, Buddy
 
 admin.site.register(Dive)
 admin.site.register(Note)
+admin.site.register(Buddy)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.urls import reverse
 
+class Buddy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('buddies_detail', kwargs={'pk': self.id})
+
 
 class Dive(models.Model):
     number = models.IntegerField('Dive Number')
