3dd5b0e57801c19565e877ce6417ea8af6b3ee29
jwcrosby <jwcrosby89@gmail.com>
2021-08-26 13:40:20 -0600

    add Trip model to admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Dive, Note, Buddy, Photo
+from .models import Dive, Photo, Note, Buddy, Trip
 
 admin.site.register(Dive)
+admin.site.register(Photo)
 admin.site.register(Note)
 admin.site.register(Buddy)
-admin.site.register(Photo)
+admin.site.register(Trip)
