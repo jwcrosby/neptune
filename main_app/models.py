6dd7887fedcafdee7f1a4b9e8c459b9b7ede0840
jwcrosby <jwcrosby89@gmail.com>
2021-08-25 22:16:49 -0600

    add photo model

--- main_app/models.py
+++ main_app/models.py
@@ -38,3 +38,11 @@ class Note(models.Model):
 
     # class Meta:
         # ordering = ['-date']
+
+
+class Photo(models.Model):
+    url = models.CharField(max_length=250)
+    dive = models.OneToOneField(Dive, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for dive_id: {self.dive_id} @{self.url}"
