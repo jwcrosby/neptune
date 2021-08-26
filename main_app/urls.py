7e58a473c070caf22b0d12ab2b93bbc69b7e9800
jwcrosby <jwcrosby89@gmail.com>
2021-08-25 20:49:22 -0600

    add buddy create path

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('dives/<int:pk>/update/', views.DiveUpdate.as_view(), name='dives_update'),
     path('dives/<int:pk>/delete/', views.DiveDelete.as_view(), name='dives_delete'),
     path('dives/<int:dive_id>/add_note/', views.add_note, name='add_note'),
+    path('buddies/create/', views.BuddyCreate.as_view(), name='buddies_create'),
 ]
