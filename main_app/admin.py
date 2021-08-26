ecbd3b4ab618b9a8e402e3e6ade59522737bdc0f
jwcrosby <jwcrosby89@gmail.com>
2021-08-25 22:37:38 -0600

    add Photo to admin panel

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Dive, Note, Buddy
+from .models import Dive, Note, Buddy, Photo
 
 admin.site.register(Dive)
 admin.site.register(Note)
 admin.site.register(Buddy)
+admin.site.register(Photo)
